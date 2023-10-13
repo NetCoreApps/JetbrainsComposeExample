9d071544b27256f5e9384656724cf835d40f5171
Darren Reid <darren@reidmail.org>
2023-10-13 13:02:13 +1100

    Improve visuals.

--- compose-demo/src/main/kotlin/Main.kt
+++ compose-demo/src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +21,7 @@ import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -33,7 +40,7 @@ fun App() {
     trustSelfSignedSSL()
     val client = JsonServiceClient("https://localhost:5001")
     var textFieldState by remember { mutableStateOf(TextFieldValue()) }
-    var searchResult by remember { mutableStateOf("") }
+    var searchResults by remember { mutableStateOf(listOf<String>()) }
     val coroutineScope = rememberCoroutineScope()
 
     // Function to call ServiceStack API
@@ -42,7 +49,7 @@ fun App() {
             val request = SearchFiles()
             request.pattern = textFieldState.text
             val response: SearchFilesResponse = client.get(request)
-            searchResult = response.results.joinToString("\n")
+            searchResults = response.results
         }
     }
 
@@ -62,7 +69,7 @@ fun App() {
                 .background(lightBlue),  // Use custom light blue color
             contentAlignment = Alignment.Center
         ) {
-            Text("My Application", color = Color.White, style = TextStyle(fontSize = 24.sp))
+            Text("My Application", color = Color.White, style = TextStyle(fontSize = 2.em))
         }
 
         Box(
@@ -89,29 +96,36 @@ fun App() {
                     textFieldState = it
                     callService()
                 },
-                textStyle = TextStyle(color = Color.Black),
+                textStyle = TextStyle(color = Color.Black, fontSize = 2.em),
                 modifier = Modifier
                     .fillMaxWidth(fraction = 0.8f)
                     .height(40.dp)
-                    .padding(top = 4.dp)
+                    .padding(top = 4.dp, start = 4.dp)
                     .border(2.dp, lightBlue, RoundedCornerShape(4.dp))
                     .clip(RoundedCornerShape(4.dp))
             )
         }
 
-        Box(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 4.dp),  // Padding on the top, start, and end
-            contentAlignment = Alignment.Center  // Centers the content horizontally
+                .padding(16.dp)
         ) {
-
-            // Search Result
-            Text(
-                text = searchResult,
-                color = Color.Blue,
-                modifier = Modifier.padding(16.dp)
-            )
+            items(searchResults) { filePath ->
+                // Display each file path
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = null, tint = Color.Blue)
+                    Spacer(modifier = Modifier.width(8.dp)) // Gives some space between the icon and the text
+                    Text(filePath,
+                        color = Color.Black,
+                        style = TextStyle(fontSize = 16.sp)
+                    )
+                }
+                Divider(modifier = Modifier.padding(vertical = 8.dp)) // Adds a divider between items
+            }
         }
     }
 }
